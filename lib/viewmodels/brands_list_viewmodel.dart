2314435722a9f8d5d8c9887b8c5791117506a27d
ayna-ai <151538108+ayna-ai@users.noreply.github.com>
2023-12-02 13:41:55 +0530

    Adding BrandsList ViewModel

---
+++ lib/viewmodels/brands_list_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/flutter.dart';
+import 'lib/models/brand.dart';
+
+import 'lib/services/brand_service.dart';
+
+class BrandsListViewModel {
+  Future<List<Brand>> getBrands() async {
+    // Todo* Implement logic to fetch brands from backend
+  }
+}
