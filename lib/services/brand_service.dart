326a2f82625fb73d78b18f756d27043149ae6c96
ayna-ai <151538108+ayna-ai@users.noreply.github.com>
2023-12-02 13:42:21 +0530

    Adding BrandService

---
+++ lib/services/brand_service.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+import 'package:flutter/flutter.dart';
+
+class BrandService {
+  Future<List<String>> getBrands() async {
+    // Todo* Implement logic to fetch brands from backend
+  }
+}
