df97f1e52e85e712e03af98c719167002531d6b6
ayna-ai <151538108+ayna-ai@users.noreply.github.com>
2023-12-02 13:40:50 +0530

    Adding Photo model

---
+++ lib/models/photo.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:flutter/flutter.dart';
+
+class Photo {
+  final String id;
+  final String url;
+  final String brandId;
+
+  Photo({this.id, this.url, this.brandId});
+}
